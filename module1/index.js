9111bd2353d843205d3bf2bb245eab94ae4194da
maclararose <mcbarroso.contato@gmail.com>
2021-02-24 02:05:18 -0300

    Module 1

---
+++ module1/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const cursos = ['NodeJs', 'VueJs', 'ReactJs', '.NET', 'C#', 'Java'];
+
+//Query params = ?nome=NodeJs
+//Route params = /curso/2
+//Request body = { nome: 'JavaScript', tipo: 'Backend'}
+
+server.get('/cursos', (req, res) => {
+    return res.json(cursos);
+});
+
+server.get('/cursos/:index', (req, res) => {
+    const {index} = req.params;
+
+    return res.json(cursos[index]);
+});
+
+server.post('/cursos', (req, res) =>{
+    const { name } = req.body;
+    cursos.push(name);
+
+    return res.json(cursos);
+});
+
+server.put('/cursos/:index', (req, res) =>{
+    const {index} = req.params;
+    const {name} = req.body;
+
+    cursos[index] = name;
+
+    return res.json(cursos);
+});
+
+server.delete('/cursos/:index', (req, res) =>{
+    const {index} = req.params;
+
+    cursos.splice(index, 1);
+
+    return res.json(cursos);
+})
+
+server.listen(8080);
